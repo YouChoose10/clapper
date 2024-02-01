e5a24b8794193fcdd7bb43365b353c90b2bb363f
YouChoose10 <152621273+YouChoose10@users.noreply.github.com>
2024-02-01 13:03:42 -0500

    made the program

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+def on_sound_loud():
+    claps += 1
+    if claps == 2 and isOn:
+        pins.digital_write_pin(DigitalPin.P0, 0)
+        claps = 0
+    elif claps == 2 and not isOn:
+        pins.digital_write_pin(DigitalPin.P0, 1)
+        claps = 0
+        
+input.on_sound(DetectedSound.LOUD, on_sound_loud)
+
+claps = 0
+isOn = False
 serial.redirect_to_usb()
 
 def on_forever():
--- main.ts
+++ main.ts
@@ -1,4 +1,19 @@
+input.onSound(DetectedSound.Loud, function () {
+    claps += 1
+    if (claps == 2 && isOn) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        claps = 0
+        isOn = false
+    } else if (claps == 2 && !(isOn)) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        isOn = true
+        claps = 0
+    }
+})
+let isOn = false
+let claps = 0
+claps = 0
 serial.redirectToUSB()
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
+	
 })
